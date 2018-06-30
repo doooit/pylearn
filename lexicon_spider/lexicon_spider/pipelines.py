e8673c692cff5a99d1bafdf42385807fd997d603
yylnascent <yylnascent@126.com>
2018-06-30 10:34:23 +0800

    save lexicon

--- lexicon_spider/lexicon_spider/pipelines.py
+++ lexicon_spider/lexicon_spider/pipelines.py
@@ -8,7 +8,17 @@
 import os
 import requests
 
+def assure_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 class LexiconSpiderPipeline(object):
     def process_item(self, item, spider):
-        print(item)
+        dict_path = os.path.join('.', item['main_cate_name'], item['sub_cate_name'])
+        dict_file = os.path.join('.', item['main_cate_name'], item['sub_cate_name'], '%s.scel'%item['dict_name'])
+        assure_exists(dict_path)
+        ret = requests.get(item['download_url'])
+        if str(ret.status_code) == '200':
+            with open(dict_file, 'wb') as fp:
+                fp.write(ret.content)
         return item
